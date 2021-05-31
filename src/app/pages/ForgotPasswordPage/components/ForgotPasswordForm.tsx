1cee6b5a5265358965241dd923cc336346e14c96
Dang Dang <dat.dang@bigtrees.tech>
2021-05-31 22:00:43 +0700

    Split forgot password form from the forgot password page, add toast message when forgot password

---
+++ src/app/pages/ForgotPasswordPage/components/ForgotPasswordForm.tsx
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import styled from 'styled-components/macro';
+import { StyleConstants } from 'styles/StyleConstants';
+
+import { Input, Form, Button, Row } from 'antd';
+import { Auth } from 'aws-amplify';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+export function ForgotPasswordForm(props) {
+  const [requestedResetPassword, setRequestedResetPassword] = React.useState(false);
+
+  const [email, setEmail] = React.useState('');
+
+  const history = useHistory();
+
+  const onFinish = (values) => {
+    const { email } = values;
+    
+    setEmail(email);
+    sendForgotPasswordCode(email);
+  }
+
+  const sendForgotPasswordCode = (email) => {
+    Auth.forgotPassword(email)
+      .then(data => setRequestedResetPassword(true))
+      .catch(err => toast.error(err.message));
+  }
+
+  const onSubmitPasswordReset = (values) => {
+    const { code, newPassword } = values;
+    Auth.forgotPasswordSubmit(email, code, newPassword)
+      .then(data => {
+        history.push('/signin')
+        toast.success('Your password has been changed, you can login now.');
+      })
+      .catch(err => toast.error(err.message))
+  }
+
+  return (
+    <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
+      {!requestedResetPassword ? (
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="Your email"
+            name="email"
+            rules={[{ required: true, type: 'email' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" style={{ float: 'right' }}>
+              Recover password
+        </Button>
+          </Form.Item>
+        </Form>
+      ) : (
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onSubmitPasswordReset}
+        >
+          <Form.Item
+            label="Code"
+            name="code"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="New password"
+            name="newPassword"
+            rules={[{ required: true }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" style={{ float: 'right' }}>
+              Change password
+        </Button>
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <div style={{ marginTop: '10px' }}>
+              <span> Could not receive the code? <a onClick={() => sendForgotPasswordCode(email)}>resend</a></span>
+            </div>
+          </Form.Item>
+        </Form>
+      )}
+    </Row>
+  );
+}
+
+const Wrapper = styled.div`
+  height: calc(100vh - ${StyleConstants.NAV_BAR_HEIGHT});
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 320px;
+`;
+
+const Title = styled.div`
+  margin-top: -8vh;
+  font-weight: bold;
+  color: ${p => p.theme.text};
+  font-size: 3.375rem;
+
+  span {
+    font-size: 3.125rem;
+  }
+`;
