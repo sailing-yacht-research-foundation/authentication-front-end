05f14d27966f61d8804e779ec0e39c9b2bd0009a
Dang Dang <dat.dang@bigtrees.tech>
2021-07-16 20:26:28 +0700

    SNS-224 [Add] Add amplify production pull script

---
+++ amplify-pull-production.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"build\",\
+\"BuildCommand\":\"yarn build\",\
+\"StartCommand\":\"yarn start\"\
+}"
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"AKIAU4MUOS3JNYGU6XGC\",\
+\"secretAccessKey\":\"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\",\
+\"region\":\"us-west-2\"\
+}"
+AMPLIFY="{\
+\"projectName\":\"syrf\",\
+\"appId\":\"d29rwzm6pkx02t\",\
+\"envName\":\"pro\",\
+\"defaultEditor\":\"code\"\
+}"
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react\",\
+\"config\":$REACTCONFIG\
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify pull \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
