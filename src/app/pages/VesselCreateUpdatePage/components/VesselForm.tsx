ca29d8e0002e1212c3d256f4f63d0463feb7cb52
Dang Dang <dat.dang@bigtrees.tech>
2021-09-20 17:47:22 +0700

    SNS-228 [Resolve] Resolve unresolved files from last merge

--- src/app/pages/VesselCreateUpdatePage/components/VesselForm.tsx
+++ src/app/pages/VesselCreateUpdatePage/components/VesselForm.tsx
@@ -112,11 +112,7 @@ export const VesselForm = () => {
                 showDeleteModal={showDeleteModal}
                 setShowDeleteModal={setShowDeleteModal}
             />
-<<<<<<< HEAD
             <PageHeaderContainerResponsive style={{ 'alignSelf': 'flex-start', width: '100%' }}>
-=======
-            <PageHeaderContainer style={{ 'alignSelf': 'flex-start', width: '100%' }}>
->>>>>>> develop
                 <PageHeaderText>{mode === MODE.UPDATE ? t(translations.vessel_create_update_page.update_your_vessel) : t(translations.vessel_create_update_page.create_a_new_vessel)}</PageHeaderText>
                 <Space size={10}>
                     <CreateButton onClick={() => history.push("/vessels")} icon={<BsCardList
@@ -127,11 +123,7 @@ export const VesselForm = () => {
                         size={18} />}>{t(translations.vessel_create_update_page.delete)}</DeleteButton>}
 
                 </Space>
-<<<<<<< HEAD
             </PageHeaderContainerResponsive>
-=======
-            </PageHeaderContainer>
->>>>>>> develop
             <SyrfFormWrapper>
                 <Spin spinning={isSaving}>
                     <Form
