88ed528777f158821626311ae89ba35d383289af
Satrio Adi <45205758+satrioadii@users.noreply.github.com>
2021-09-23 19:53:51 +0700

    Feature/sns 359 (#40)
    * SNS-328 [Add] Get scraped race detail by id service
    
    * SNS-328 [Add] Get scraped race detail redux
    
    * SNS-328 [Add] Playback types
    
    * SNS-359 [Add] Leaderboard component
    
    * SNS-328 [Update] Manage state for streaming race and scraped race
    
    * SNS-328 [Add] Ant design icon package
    
    * SNS-328 [Update] Use raceId instead of searchRaceId and make sure the dynamic size of map work
    
    * SNS-328 [Update] Change raceId to raceid on query string
    
    * SNS-328 [Update] fixed scraped iframe UI issue
    
    * SNS-328 [Update] Improve map resize implementation
    
    * SNS-328 [Update] Fixed iframe and back button issues

--- src/app/pages/PlaybackPage/components/slice/saga.ts
+++ src/app/pages/PlaybackPage/components/slice/saga.ts
@@ -4,41 +4,54 @@
 
 import { all, call, put, select, takeLatest } from "@redux-saga/core/effects";
 import { message } from "antd";
-import { getCompetitionUnitById } from "services/live-data-server/competition-units";
+import { getCompetitionUnitById, searchScrapedRaceById } from "services/live-data-server/competition-units";
 import { getVesselParticipantGroupById } from "services/live-data-server/vessel-participant-group";
 import { playbackActions } from ".";
 import { selectCompetitionUnitDetail } from "./selectors";
 
 export function* getCompetitionUnitDetail({ type, payload }) {
-  const { id } = payload;
+    const { id } = payload;
 
-  const result = yield call(getCompetitionUnitById, id);
+    const result = yield call(getCompetitionUnitById, id);
 
-  if (result.success) {
-    yield put(playbackActions.setCompetitionUnitId(id));
-    yield put(playbackActions.setCompetitionUnitDetail(result.data));
-  } else {
-    message.error('Competition unit id not found!')
-  }
+    if (result.success) {
+        yield put(playbackActions.setCompetitionUnitId(id));
+        yield put(playbackActions.setCompetitionUnitDetail(result.data));
+    } else {
+        message.error("Competition unit id not found!");
+    }
 }
 
 export function* getVesselParticipants() {
-  const competitionUnitDetail = yield select(selectCompetitionUnitDetail);
-  const { vesselParticipantGroupId } = competitionUnitDetail;
+    const competitionUnitDetail = yield select(selectCompetitionUnitDetail);
+    const { vesselParticipantGroupId } = competitionUnitDetail;
 
-  const { data } = yield call(getVesselParticipantGroupById, vesselParticipantGroupId);
-  yield put(playbackActions.setVesselParticipants(data.vesselParticipants));
-};
+    const { data } = yield call(getVesselParticipantGroupById, vesselParticipantGroupId);
+    yield put(playbackActions.setVesselParticipants(data.vesselParticipants));
+}
+
+export function* getSearchRaceDetail({ type, payload }) {
+    const { searchRaceId } = payload;
+    if (!searchRaceId) return message.error("Race not found!");
+    const result = yield call(searchScrapedRaceById, searchRaceId);
+
+    if (result.success) {
+        yield put(playbackActions.setSearchRaceId(searchRaceId));
+
+        // Select the first data
+        const raceDetail = result.data.hits?.hits?.[0];
+        if (!raceDetail) return message.error("Race not found!");
+
+        yield put(playbackActions.setSearchRaceDetail(raceDetail._source));
+    } else {
+        message.error(result.error.message || "Something went wrong when try to search the race id");
+    }
+}
 
 export default function* playbackSaga() {
-  yield all([
-    takeLatest(
-      playbackActions.getCompetitionUnitDetail.type,
-      getCompetitionUnitDetail
-    ),
-    takeLatest(
-      playbackActions.getVesselParticipants.type,
-      getVesselParticipants
-    )
-  ]);
+    yield all([
+        takeLatest(playbackActions.getCompetitionUnitDetail.type, getCompetitionUnitDetail),
+        takeLatest(playbackActions.getVesselParticipants.type, getVesselParticipants),
+        takeLatest(playbackActions.getSearchRaceDetail.type, getSearchRaceDetail),
+    ]);
 }
