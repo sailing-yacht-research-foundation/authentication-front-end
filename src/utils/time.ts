2a5d7e085d5b969391b55c928f3405cdef0e8945
satrioadi <satriooadi@gmail.com>
2021-10-19 09:36:04 +0700

    SNS-422 [Update] implementation on millis to hours

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,11 @@
 export const timeMillisToHours = (millis: number | undefined): string => {
   if (!millis) return "00:00:00";
 
-  const currentSecond = Math.round(millis / 1000);
+  const currentSecond = Math.floor(millis / 1000);
 
-  let hour: number | string = Math.round(currentSecond / 3600);
-  let minute: number | string = Math.round((currentSecond % 3600) / 60);
-  let second: number | string = currentSecond % 60;
+  const hour: string = `${Math.floor(currentSecond / 3600)}`;
+  const minute: string = `${Math.floor((currentSecond % 3600) / 60)}`;
+  const second: string = `${currentSecond % 60}`;
 
-  if (hour < 10) hour = `0${hour}`;
-  if (minute < 10) minute = `0${minute}`;
-  if (second < 10) second = `0${second}`;
-
-  return `${hour}:${minute}:${second}`;
+  return `${hour.padStart(2, "0")}:${minute.padStart(2, "0")}:${second.padStart(2, "0")}`;
 };
