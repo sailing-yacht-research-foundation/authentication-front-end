500fd658f021bb2db1183dd44a8d4e1c22ed23f0
Dang Dang <dat.dang@bigtrees.tech>
2021-10-27 09:20:23 +0700

    SNS-471 [Move] Move curtains background to login page

--- src/app/pages/AboutPage/index.tsx
+++ src/app/pages/AboutPage/index.tsx
@@ -3,16 +3,13 @@ import * as React from 'react';
 import { BenefitCarousel } from './components/BenefitCarousel';
 import { Footer } from './components/Footer';
 import { VideoSection } from './components/VideoSection';
-import { Curtains } from "react-curtains";
 
 export function AboutPage() {
   return (
-    <Curtains pixelRatio={Math.min(1.5, window.devicePixelRatio)}>
       <Wrapper>
         <VideoSection />
         <BenefitCarousel />
         <Footer />
       </Wrapper>
-    </Curtains>
   );
 }
