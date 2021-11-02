158279a7b6c78542149f99a8181f647e8190d4ff
Dang Dang <dat.dang@bigtrees.tech>
2021-11-02 20:27:38 +0700

    SNS-523 [Update] Update env variables

--- src/services/service-constants.ts
+++ src/services/service-constants.ts
@@ -4,5 +4,5 @@ export const SYRF_SERVER = {
     API_URL:process.env.REACT_APP_SYRF_API_URL,
     API_VERSION: process.env.REACT_APP_SYRF_API_VERSION,
     DEV_TOKEN: process.env.REACT_APP_SYRF_API_DEV_TOKEN,
-    EXPEDITION_SERVER: 'https://streaming-server-dev.syrf.io'
+    EXPEDITION_SERVER: process.env.REACT_APP_SYRF_STREAMING_SERVER_URL
 }
