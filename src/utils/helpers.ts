85ea260523d586a733272a2b2100f4473bc7a6d1
Dang Dang <dat.dang@bigtrees.tech>
2021-11-16 22:28:05 +0700

    SNS-550 [Fix] Change to padstart instead of if else

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -32,10 +32,10 @@ export const milisecondsToMinutes = (duration) => {
     let seconds: any = Math.floor((duration / 1000) % 60),
         minutes: any = Math.floor((duration / (1000 * 60)) % 60),
         hours: any = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+        
+        hours = hours.toString().padStart(2, '0');
+        minutes= minutes.toString().padStart(2, '0');
+        seconds= seconds.toString().padStart(2, '0');
 
     return hours + ":" + minutes + ":" + seconds;
 }
