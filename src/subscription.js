7d7b0d1efd73358a13a88354ef77534b49d16c2c
Dang Dang <dat.dang@bigtrees.tech>
2022-03-07 19:04:23 +0700

    SNS-772 [Fix] Fix failing test

--- src/subscription.js
+++ src/subscription.js
@@ -1,6 +1,7 @@
 const convertedVapidKey = urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY)
 
 function urlBase64ToUint8Array(base64String) {
+  // eslint-disable-next-line
   const padding = "=".repeat((4 - base64String.length % 4) % 4)
   // eslint-disable-next-line
   const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
@@ -27,22 +28,22 @@ function sendSubscription(subscription) {
 
 export function subscribeUser() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(registration) {
+    navigator.serviceWorker.ready.then(function (registration) {
       if (!registration.pushManager) {
         console.log('Push manager unavailable.')
         return
       }
 
-      registration.pushManager.getSubscription().then(function(existedSubscription) {
+      registration.pushManager.getSubscription().then(function (existedSubscription) {
         if (existedSubscription === null) {
           console.log('No subscription detected, make a request.')
           registration.pushManager.subscribe({
             applicationServerKey: convertedVapidKey,
             userVisibleOnly: true,
-          }).then(function(newSubscription) {
+          }).then(function (newSubscription) {
             console.log('New subscription added.')
             sendSubscription(newSubscription)
-          }).catch(function(e) {
+          }).catch(function (e) {
             if (Notification.permission !== 'granted') {
               console.log('Permission was not granted.')
             } else {
@@ -55,7 +56,7 @@ export function subscribeUser() {
         }
       })
     })
-      .catch(function(e) {
+      .catch(function (e) {
         console.error('An error ocurred during Service Worker registration.', e)
       })
   }
