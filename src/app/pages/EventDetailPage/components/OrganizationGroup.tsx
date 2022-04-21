689c95aa1ea345c7d90be7e02edc31cd48aae2f5
Dang Dang <dat.dang@bigtrees.tech>
2022-04-21 15:13:50 +0700

    SNS-933 [Add] Add logic to show/hide the organization if exists

--- src/app/pages/EventDetailPage/components/OrganizationGroup.tsx
+++ src/app/pages/EventDetailPage/components/OrganizationGroup.tsx
@@ -9,26 +9,29 @@ import { CalendarEvent } from 'types/CalendarEvent';
 import { DEFAULT_GROUP_AVATAR } from 'utils/constants';
 import { Link } from 'react-router-dom';
 
-export const OrganizationGroup = ({ event }: { event: Partial<CalendarEvent>}) => {
+export const OrganizationGroup = ({ event }: { event: Partial<CalendarEvent> }) => {
 
     const { t } = useTranslation();
 
     const organization = event.organizerGroupDetail;
 
-    return (<>
-        <PageHeaderContainer>
-            <PageHeaderTextSmall>{t(translations.event_detail_page.planning_organization)}</PageHeaderTextSmall>
-        </PageHeaderContainer>
-        <Card
-            style={{ width: 300 }}
-            actions={[
-                <Link to={`/groups/${event.organizerGroupId}`}><IoLinkOutline key="view" /> {t(translations.event_detail_page.view_organiztion)}</Link>,
-            ]}
-        >
-            <Meta
-                avatar={<Avatar src={organization?.groupImage || DEFAULT_GROUP_AVATAR} />}
-                title={organization?.groupName}
-            />
-        </Card>
-    </>);
+    if (event.organizerGroupId)
+        return (<>
+            <PageHeaderContainer>
+                <PageHeaderTextSmall>{t(translations.event_detail_page.planning_organization)}</PageHeaderTextSmall>
+            </PageHeaderContainer>
+            <Card
+                style={{ width: 300 }}
+                actions={[
+                    <Link to={`/groups/${event.organizerGroupId}`}><IoLinkOutline key="view" /> {t(translations.event_detail_page.view_organiztion)}</Link>,
+                ]}
+            >
+                <Meta
+                    avatar={<Avatar src={organization?.groupImage || DEFAULT_GROUP_AVATAR} />}
+                    title={organization?.groupName}
+                />
+            </Card>
+        </>);
+
+    return <></>;
 }
