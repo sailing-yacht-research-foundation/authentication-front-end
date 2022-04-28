e604f189423075550878de5fa57372892ca35050
Dang Dang <dat.dang@bigtrees.tech>
2022-04-28 17:34:31 +0700

    SNS-901 [Update] Update code comment about the _changeBoatColorToGray function

--- src/app/pages/PlaybackPage/components/RaceMap.tsx
+++ src/app/pages/PlaybackPage/components/RaceMap.tsx
@@ -127,6 +127,7 @@ export const RaceMap = (props) => {
     const boats = current.boats;
     if (!boats[vesselParticipantId]) return;
     // There is a listener that checks for last time the boat pings. When the boat didn't ping for more than 60secs it would be grayed out.
+    // Line 132 makes the boat turn gray immediately.
     // please check _updateBoatColorIfPingNotReceived and _initLayerAndSetLocationAndHeadingForBoat to understand the flow.
     boats[vesselParticipantId].lastPing = Date.now() - 60000; 
     boats[vesselParticipantId].layer._icon.firstElementChild.style.fill = colors.GRAY;
