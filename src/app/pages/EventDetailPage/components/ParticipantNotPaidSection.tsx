b46f87d743e909bb453a77e6ba72d412002425c1
Dang Dang <dat.dang@bigtrees.tech>
2022-05-06 14:37:40 +0700

    SNS-941 [Add] Add pay now translation text

--- src/app/pages/EventDetailPage/components/ParticipantNotPaidSection.tsx
+++ src/app/pages/EventDetailPage/components/ParticipantNotPaidSection.tsx
@@ -51,7 +51,7 @@ export const ParticipantNotPaidSection = ({ event }: { event: Partial<CalendarEv
                     height={150}
                     width={150} />
                 <LottieMessage>{t(translations.event_detail_page.hey_participant_name_this_event_has_participant_fee, { participantingFee: event.participatingFee, participantName: getUserName(authUser) })}</LottieMessage>
-                <BorderedButton type="primary" onClick={pay}>Pay Now</BorderedButton>
+                <BorderedButton type="primary" onClick={pay}>{t(translations.event_detail_page.pay_now)}</BorderedButton>
             </LottieWrapper>
         );
 
