6c37be36e912c0ff58b096de01180fbb5e518915
Dang Dang <dat.dang@bigtrees.tech>
2022-06-17 22:21:45 +0700

    Add click to replay button

--- src/app/pages/PlaybackPage/components/ModalRacePostponed.tsx
+++ src/app/pages/PlaybackPage/components/ModalRacePostponed.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Button, Modal } from "antd";
 import { translations } from "locales/translations";
-import { handleGoBack } from "utils/helpers";
-import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { selectCompetitionUnitDetail } from "./slice/selectors";
 import moment from "moment";
 
 export const ModalRacePostponed = () => {
-
-    const history = useHistory();
 
     const { t } = useTranslation();
 
@@ -25,7 +21,7 @@ export const ModalRacePostponed = () => {
                     <h3>{t(translations.playback_page.race_is_postponed)}</h3>
                     <span>{t(translations.playback_page.this_race_is_postponed_please_check_with_event_creator)}</span>
                     <br />
-                    <Button onClick={() => handleGoBack(history)} type="link">{t(translations.playback_page.go_back)}</Button>
+                    <Button onClick={() => window.location.reload()} type="link">{t(translations.playback_page.click_here_to_replay)}</Button>
                 </p>
             </Modal>
         </div>
