d5733e1b554f430c055a804dc245d22a1678233b
datdang2412 <49665252+datdang2412@users.noreply.github.com>
2022-08-01 17:49:25 +0700

    Bugfix/sns 1216 (#307)
    * SNS-1216 [Fix] Make the completed race modal closable
    
    * SNS-1216 [Add] Add dispatch to oldrace playback type
    
    * SNS-1216 [Add] Add modal race is being processed, add interval getting data
    
    * SNS-1216 [Add] Add clearinterval when the process is done
    
    * SNS-1216 [Fix] Fix the flow, remove setTimeout.
    
    * SNS-1216 [Add] breakline between import
    
    * SNS-1216 [Add] Add renew data to see if the playback can play immediately
    
    * SNS-1216 [Add] Remove duplicate code
    
    * SNS-1216 Update submodule

---
+++ src/app/pages/PlaybackPage/components/ModalDataIsBeingProcessed.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Modal } from "antd";
+import { translations } from "locales/translations";
+import { RaceStatusModalWrapper } from "app/components/SyrfGeneral";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCompetitionUnitDetail, selectPlaybackType } from "./slice/selectors";
+import { RaceSource, RaceStatus } from "utils/constants";
+import { useTranslation } from "react-i18next";
+import { usePlaybackSlice } from "./slice";
+import { PlaybackTypes } from "types/Playback";
+
+let reloadInterval;
+const reloadEvery = 30000; // ms
+
+export const ModalDataIsBeingProcessed = () => {
+
+    const [showModal, setShowModal] = React.useState<boolean>(false);
+
+    const competitionUnitDetail = useSelector(selectCompetitionUnitDetail);
+
+    const dispatch = useDispatch();
+
+    const { actions } = usePlaybackSlice();
+
+    const { t } = useTranslation();
+
+    const playbackType = useSelector(selectPlaybackType);
+
+    React.useEffect(() => {
+        if (competitionUnitDetail.calendarEvent?.source === RaceSource.SYRF
+            && competitionUnitDetail.status === RaceStatus.COMPLETED) {
+            clearIntervalIfNecessary();
+            if (!competitionUnitDetail.isSavedByEngine) {
+                dispatch(actions.setPlaybackType(PlaybackTypes.RACELOADING));
+                setShowModal(true);
+                reloadInterval = setInterval(() => {
+                    dispatch(actions.setPlaybackType(PlaybackTypes.RACELOADING));
+                    dispatch(actions.getCompetitionUnitDetail({ id: competitionUnitDetail.id }));
+                }, reloadEvery);
+            } else {
+                if (playbackType !== PlaybackTypes.OLDRACE) {
+                    dispatch(actions.setPlaybackType(PlaybackTypes.OLDRACE));
+                }
+
+                setShowModal(false);
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [competitionUnitDetail.source, competitionUnitDetail.isSavedByEngine]);
+
+    const clearIntervalIfNecessary = () => {
+        if (reloadInterval) {
+            clearInterval(reloadInterval);
+        }
+    }
+
+    return (
+        <div style={{ zIndex: 999 }}>
+            <Modal visible={showModal} footer={null} closable={false}>
+                <RaceStatusModalWrapper>
+                    <h3>{t(translations.playback_page.we_are_processing_data)}</h3>
+                    <span>{t(translations.playback_page.it_will_take_some_time_to_process_this_race)}</span>
+                </RaceStatusModalWrapper>
+            </Modal>
+        </div>
+    );
+}
